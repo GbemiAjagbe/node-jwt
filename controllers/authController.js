6092fe8bd8e1ce5855945a3a4b167fef130de170
GbemiAjagbe <dajagbe357@stu.ui.edu.ng>
2022-12-22 17:38:06 -0600

    login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,16 @@ const handleErrors = (err) => {
   console.log(err.message, err.code);
   let errors = { email: "", password: "" };
 
+  //Incorrect email
+  if (err.message === "incorrect email") {
+    errors.email = "that email is not registered";
+  }
+
+  //Incorrect password
+  if (err.message === "incorrect password") {
+    errors.email = "that password is incorrect";
+  }
+
   //Dupllicate error code
   if ((err.code = 11000)) {
     errors.email = "that email is already registered";
@@ -23,7 +33,7 @@ const handleErrors = (err) => {
 
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (id) => {
-  return jwt.sign({ id }, 'string secret', {
+  return jwt.sign({ id }, "string secret", {
     expiresIn: maxAge,
   });
 };
@@ -38,8 +48,8 @@ module.exports.signup_post = async (req, res) => {
   try {
     const user = await User.create({ email, password });
     const token = createToken(user._id);
-    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000});
-    res.status(201).json({user: user._id});
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
@@ -47,9 +57,19 @@ module.exports.signup_post = async (req, res) => {
 };
 
 module.exports.login_get = (req, res) => {
-    res.render('login')
-}
+  res.render("login");
+};
 
-module.exports.login_post = (req, res) => {
-    res.send('new signup')
-}
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({errors});
+  }
+};
