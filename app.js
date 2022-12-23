f5a6739bfc95fd7ee738b09217f92dca90d94fdd
GbemiAjagbe <dajagbe357@stu.ui.edu.ng>
2022-12-22 19:09:59 -0600

    logout & conditional rendering

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
-const { requireAuth } = require('./middleware/authMiddleware');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -24,6 +24,7 @@ app.set('view engine', 'ejs');
 
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
